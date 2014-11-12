2f602b13c513341af20e49bac7cb9617227d2ccd
Matthew <Matthew@Matthew>
2014-11-11 20:40:38 -0600

    again

--- PokerBot/src/GameState.java
+++ PokerBot/src/GameState.java
@@ -1,4 +1,19 @@
+import java.awt.Robot;
+
 
 public class GameState {
-
+	public static void main(String [] args) {
+		
+		getHoleCards();
+		
+	}
+	
+	//Reads Card identification squares 
+	public static void getHoleCards() throws AWTException{
+		
+		Robot robot = new Robot();
+		int[] 
+		robot.getPixelColor(x, y).getGreen();
+		
+	}
 }
